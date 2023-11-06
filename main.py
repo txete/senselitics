79b56f8e7c646c7aa9420021c7c6c72d9cba29f4
Txete <inaki.demon@gmail.com>
2023-11-06 17:49:42 +0100

    correcciones para trabajar

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
-from pyspark.sql import Row
-import json
+# from pyspark.sql import Row
+# import json
 
 def save_to_mongo(rdd):
     if not rdd.isEmpty():
         df = spark.createDataFrame(rdd)
         df.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
 
+# Verificar si la ip ha cambiado en la maquina
 spark = SparkSession.builder \
     .appName("TwitterStream") \
-    .config("spark.mongodb.input.uri", "mongodb://192.168.20.87/ZIPS.ZIP") \
-    .config("spark.mongodb.output.uri", "mongodb://192.168.20.87/ZIPS.ZIP") \
+    .config("spark.mongodb.input.uri", "mongodb://192.168.23.32/reto2.tweets") \
+    .config("spark.mongodb.output.uri", "mongodb://192.168.23.32/reto2.tweets") \
     .getOrCreate()
 
+# nc -lk 9999 desde la BBDD para probar el streaming
 ssc = StreamingContext(spark.sparkContext, 10)
 
-lines = ssc.socketTextStream("192.168.20.87", 9999)
+lines = ssc.socketTextStream("192.168.23.32", 9999)
 lines.pprint()
 
-parsed_lines = lines.map(lambda line: Row(**json.loads(line)))
-parsed_lines.foreachRDD(save_to_mongo)
+# parsed_lines = lines.map(lambda line: Row(**json.loads(line)))
+# parsed_lines.foreachRDD(save_to_mongo)
 
 ssc.start()
 ssc.awaitTermination()
