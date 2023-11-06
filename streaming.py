81b576e124853a72526256fdf26226273fe6c090
Txete <inaki.demon@gmail.com>
2023-11-06 20:12:57 +0100

    modificar nombres para cada funcionalidad

--- streaming.py
+++ streaming.py
@@ -3,24 +3,18 @@ from pyspark.streaming import StreamingContext
 import pymongo
 import json
 
-# Define a function to save each RDD to MongoDB using PyMongo
 def save_rdd_to_mongo(rdd):
     if not rdd.isEmpty():
-        # Collect the data to the driver
         local_data = rdd.collect()
-        # Initialize PyMongo client
         client = pymongo.MongoClient("mongodb://192.168.23.32:27017/")
-        # Get the database and collection
         db = client["reto2"]
         collection = db["tweets"]
-        # Insert data into the collection
         for tweet_data in local_data:
             try:
                 tweet_json = json.loads(tweet_data)
                 collection.insert_one(tweet_json)
             except json.JSONDecodeError as e:
                 print("Error decoding JSON:", e)
-        # Close the connection
         client.close()
 
 # Spark session initialization
